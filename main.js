6718012461eac320454acbadd513bea889278fa8
KayhZ <148588629+KayhZ@users.noreply.github.com>
2024-09-12 21:22:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var_h1 = document.querySelector("h1");
+var_h1.innerHTML = 'secret Number';
+varP = document.querySelector('p');
+varP.innerHTML = 'escolha um numero de 0 a 10:';
+
+let count = 1;
+let limite = 10;
+let secret_number = parseInt(Math.random()* limite+1);
+
+function verificarChute(){
+    let guess = document.querySelector('input').value;
+    if(secret_number == guess){
+        var_h1.innerHTML = 'voce acertou!'
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        varP.innerHTML = `voce gastou ${count} tentativa`;
+    }else{
+        if(guess > secret_number){
+            varP.innerHTML = 'o numero é menor';
+        } else {
+            varP.innerHTML = 
+            'o numero é maior';
+        }
+    }
+    
+    count++;
+}
