a71a3366284cbd3d0164835257b4ca0c3e964947
KayhZ <148588629+KayhZ@users.noreply.github.com>
2024-09-12 22:30:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
+let count = 1;
+let limite = 100;
+let secret_number = parseInt(Math.random()*limite+1);
+
 var_h1 = document.querySelector("h1");
 var_h1.innerHTML = 'secret Number';
 varP = document.querySelector('p');
-varP.innerHTML = 'escolha um numero de 0 a 10:';
+varP.innerHTML = `escolha um numero de 0 a ${limite}:`;
 
-let count = 1;
-let limite = 100;
-let secret_number = parseInt(Math.random()* limite+1);
 
 function verificarChute(){
     let guess = document.querySelector('input').value;
     if(secret_number == guess){
-        let word_attempts = guess_count > 1 ? 'attempts' : 'attempt';
-        var_h1.innerHTML = 'voce acertou!'
+        let word_attempts = count > 1 ? 'tentativas' : 'tentativa';
+        var_h1.innerHTML = `Parabéns! Você venceu em ${count} ${word_attempts}.`
         document.getElementById('reiniciar').removeAttribute('disabled');
         varP.innerHTML = `voce gastou ${count} ${word_attempts}`;
     }else{
